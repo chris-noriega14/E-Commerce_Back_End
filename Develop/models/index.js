8d6d56c5a26d612fec29e573bac1d285cc80de47
chris-noriega14 <chris.noriega14@hotmail.com>
2021-10-28 23:42:32 -0500

    Added initial files, added Category,Product,ProductTag,Tag models,synced sequelize models to the database

---
+++ Develop/models/index.js
@@ -0,0 +1,29 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.hasOne(Category, {
+    
+})
+// Categories have many Products
+Category.hasMany(Product, {
+    
+})
+// Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+    
+})
+// Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+    
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
